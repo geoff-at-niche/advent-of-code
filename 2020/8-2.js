16871548a956b33259e88f952a0df8e1da788a8a
geoff-at-niche <geoff@niche.com>
2020-12-08 22:12:58 -0500

    add day 8 p2

---
+++ 2020/8-2.js
@@ -0,0 +1,36 @@
+var l = document.body.innerText.trim().split('\n')
+var altered = new Map(), found = false, flipped = false
+do {
+  var visited = new Map(), acc = 0
+  for (var i = 0; i < l.length; i++) {
+    if (visited.get(i) == true) break
+    visited.set(i, true)
+    var x = l[i].split(' ')
+    var cmd = x[0], n = Number(x[1])
+    if (cmd == 'acc') acc += n
+    else if (cmd == 'jmp') {
+      if (!flipped) {
+        var x = altered.get(i)
+        if (!x) {
+          altered.set(i, true)
+          flipped = true
+          continue
+        }
+      }
+      i += (n-1)
+    }
+    else if (cmd == 'nop') {
+      if (!flipped) {
+        var x = altered.get(i)
+        if (!x) {
+          altered.set(i, true)
+          flipped = true
+          i += (n-1)
+        }
+      }
+    }
+  }
+  if (i >= l.length) found = true
+  flipped = false
+} while (found == false)
+console.log(acc)
